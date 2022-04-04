e5571ef42e7a672e0e559d4c7938bc86bdcf97a6
Probal Ranjan Paul <probal.ranjan1198@gmail.com>
2022-04-04 17:57:26 +0600

    responsive hero box

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import Reviews from './components/Reviews/Reviews';
 import Dashboard from './components/Dashboard/Dashboard';
 import Blog from './components/Blog/Blog';
 import About from './components/About/About';
+import PageNotFound from './components/PageNotFound/PageNotFound';
 
 function App() {
   return (
-    <div className="container">
+    <div className='App container'>
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
         <Route path='/reviews' element={<Reviews />}></Route>
         <Route path='/dashboard' element={<Dashboard />}></Route>
         <Route path='/blog' element={<Blog />}></Route>
         <Route path='/about' element={<About />}></Route>
+        <Route path='*' element={<PageNotFound />}></Route>
       </Routes>
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h2>I am a home</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, ad.</p>
+        <div className='hero-container'>
+            <div className='hero-title'>
+                <h1>World's Most Favourite <br /> <span>Drone Reviewer</span></h1>
+                <p>This drone is a small folding drone with a premium 1-inch sensor camera and a number of automated flight and safety features, making it a pro-minded alternative to the more affordable Drone.</p>
+                <button>View More</button>
+            </div>
+            <div className='hero-image'>
+                <img src="./images/img1.png" alt="" />
+            </div>
         </div>
     );
 };
